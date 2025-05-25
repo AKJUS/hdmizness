f207921eb7b9c45542f7ef7f9e7685db07b2bf77
Tuetuopay <tuetuopay@me.com>
2025-05-25 17:26:22 +0200

    software: fix exceeding slow SSH login
    Dropbear has the brilliant idea to perform reverse DNS on clients for
    every single connection. I say brilliant because it's designed to run on
    embedded systems that mostly don't have access to the internet.
    
    Anyways, disable that.
    
    Also, don't generate ECDSA host keys, as they trip it up.

--- software/board/hdmizness/common/post-build.sh
+++ software/board/hdmizness/common/post-build.sh
@@ -6,7 +6,7 @@ set -x
 
 # Generate server ssh keys
 mkdir -p "${TARGET_DIR}/etc/ssh"
-for kind in dsa rsa ecdsa ed25519; do
+for kind in dsa rsa ed25519; do
     key="${TARGET_DIR}/etc/ssh/ssh_host_${kind}_key"
     if [ ! -f "$key" ]; then
         ssh-keygen -q -N "" -t $kind -f "$key"
